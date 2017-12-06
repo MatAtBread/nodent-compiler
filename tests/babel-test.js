4a0c31e600382bd89e5efecff2e60803fc94eca3
matthew.woolf@mailonline.co.uk <matthew.woolf@mailonline.co.uk>
2017-12-06 00:03:09 +0000

    Simplify & optimize AST helpers (WIP)
    Move optimized treeWalk and partialParse into arboriculture. treeWalk is
    now node-agnostic, and will walk any node-like objects.
    
    Provide interface for nodent in compiler.js
    Provide test harness for babel in tests/babel-test.js

--- tests/babel-test.js
+++ tests/babel-test.js
@@ -9,20 +9,20 @@ function printNode(ast) {
     return generate(ast,{}).code ;
 }
 
+/*
 function forEachNodeKey(n,down){
 	visitKeys[n.type].forEach(function(member){
-	    var examine = n[member] ;
-	    if (Array.isArray(examine)) {
-			examine.forEach(function(x){ if (x) down(x) }) ;
-	    } else {
-			if (examine) down(examine);
-	    }
+		if (Array.isArray(n[member])) {
+			n[member].forEach(down) ;
+		} 
+		else
+			down(n[member]);
 	}) ;
 }
 
 function nodeKeys(n) {
     return n ? visitKeys[n.type]:null ;
-}
+}*/
 
 function parse(code) {
 	return babylon.parse(code,{
@@ -32,216 +32,53 @@ function parse(code) {
 	}).program ;
 }
 
-function loc(old,repl){
-    ['start','end','loc','range'].forEach(function(k){
-        if (k in old && !(k in repl))
-            repl[k] = old[k] ;
-    }) ;
-}
-
-var referencePrototypes = {
-    replace: function(newNode) {
-        if (Array.isArray(newNode) && newNode.length===1) newNode = newNode[0] ;
-        if ('index' in this) {
-            loc(this.parent[this.field][this.index], newNode);
-            if (Array.isArray(newNode)) {
-                [].splice.apply(this.parent[this.field],[this.index,1].concat(newNode)) ;
-            } else {
-                this.parent[this.field][this.index] = newNode ;
-            }
-        } else {
-            loc(this.parent[this.field], newNode);
-            if (Array.isArray(newNode)) {
-                this.parent[this.field] = {type:'BlockStatement',body:newNode} ;
-            } else {
-                this.parent[this.field] = newNode ;
-            }
-        }
-        return this.self ;
-    },
-    append: function(newNode) {
-        if (Array.isArray(newNode) && newNode.length===1) newNode = newNode[0] ;
-        if ('index' in this) {
-            if (Array.isArray(newNode)) {
-                [].splice.apply(this.parent[this.field],[this.index+1,0].concat(newNode)) ;
-            } else {
-                this.parent[this.field].splice(this.index+1,0,newNode) ;
-            }
-        } else {
-            throw new Error("Cannot append Element node to non-array") ;
-        }
-        return this.self ;
-    },
-    index: function(){
-        return this.parent[this.field].indexOf(this.self) ;
-    },
-    removeElement: function() {
-        return this.parent[this.field].splice(this.index,1)[0] ;
-    },
-    removeNode: function() {
-        var r = this.parent[this.field] ;
-        delete this.parent[this.field] ;
-        return r ;
-    }
-};
-
-function treeWalker(n,walker,state){
-    if (!state) {
-        state = [{self:n}] ;
-        state.replace = function(pos,newNode) {
-            state[pos].replace(newNode) ;
-        }
-    }
-
-    function goDown(ref) {
-        ref.replace = referencePrototypes.replace ;
-        ref.append = referencePrototypes.append ;
-        if (ref.index) {
-            Object.defineProperties(ref, {index:{enumerable:true,get:referencePrototypes.index}}) ;
-            ref.remove = referencePrototypes.removeElement ;
-        } else {
-            ref.remove = referencePrototypes.removeNode ;
-        }
-        state.unshift(ref) ;
-        treeWalker(ref.self,walker,state) ;
-        state.shift() ;
-    }
-    
-    function descend() {
-        var keys = nodeKeys(n) ;
-        if (!keys) {
-            // We don't know what type of node this is - it's not in the ESTree spec,
-            // (maybe a 'react' extension?), so just ignore it
-        } else {
-            forEachNodeKey(n,function down(sub){
-                for (var i=0; i<keys.length; i++){
-                    var v = n[keys[i]] ;
-                    if (Array.isArray(v)) {
-                        if (v.indexOf(sub)>=0) {
-                            goDown({
-                                self:sub,
-                                parent:n,
-                                field:keys[i],
-                                index:true
-                            }) ;
-                        }
-                    } else if (v instanceof Object && sub===v) {
-                        goDown({
-                            self:sub,
-                            parent:n,
-                            field:keys[i]
-                        }) ;
-                    }
-                }
-            }) ;
-        }
-    } ;
-    walker(n,descend,state) ;
-    return n ;
-}
-
-var parseCache = {} ;
-function partialParse(code,args) {
-  if (!parseCache[code]) {
-    parseCache[code] = parse(code) ;
-  }
-
-  var result = substitute(parseCache[code]) ;
-  return {body:result.body, expr:result.body[0].type==='ExpressionStatement' ? result.body[0].expression : null} ;
-
-  /* parse and substitute:
-   * 
-   *    $1      Substitute the specified expression. If $1 occupies a slot which is an array of expressions (e.g arguments, params)
-   *            and the passed argument is an array, subtitute the whole set
-   *    {$:1}   Substitute a single statement 
-   * 
-   */
-  function substitute(src,dest) {
-    if (Array.isArray(dest) && !Array.isArray(src))
-        throw new Error("Can't substitute an array for a node") ;
-    
-    dest = dest || {} ;
-    Object.keys(src).forEach(function(k){
-      if (!(src[k] instanceof Object))
-          return dest[k] = src[k] ;
-
-      function moreNodes(v){ if (typeof v==="function") v = v() ; dest = dest.concat(v) ; return dest };
-      function copyNode(v){ if (typeof v==="function") v = v() ; dest[k] = v ; return dest };
-
-      // The src is an array, so create/grow the destination
-      // It could an an array of expressions $1,$2,$3 or statements $:1;$:2;$:3;
-      if (Array.isArray(src[k]))
-          return dest[k] = substitute(src[k],[]) ;
-
-      var p ;
-      if (Array.isArray(dest)) 
-          p = moreNodes ;
-      else
-          p = copyNode ;
-          
-      // Substitute a single identifier $.. with an expression (TODO: test provided arg is an expression node)
-      if (src[k].type==='Identifier' && src[k].name[0]==='$') 
-          return p(args[src[k].name.slice(1)]) ;
-
-      // Substitute a single labeled statement $:.. with a statement (TODO: test provided arg is a statement node)
-      if (src[k].type === 'LabeledStatement' && src[k].label.name==='$') { 
-          var spec = src[k].body.expression ;
-          return p(args[spec.name || spec.value]) ;
-      }
-
-      // Magic label to set call a function to modify a statement node  $$method: <statement>
-      // The newNode = args.method(oldNode)
-      if (src[k].type === 'LabeledStatement' && src[k].label.name.slice(0,2)==='$$') { 
-          return p(args[src[k].label.name.slice(2)](substitute(src[k]).body)) ;
-      }
-      
-      return p(substitute(src[k])) ;
-    }) ;
-    return dest ;
-  }
-}
-
 var transform = require('../lib/arboriculture').transform ;
 
-for (let fileName of process.argv.slice(2)) {
+for (var idx = 2; idx <process.argv.length; idx++) (function(){
+	var fileName = process.argv[idx] ;
 	var sample = '(async function _(){ '+fs.readFileSync(fileName).toString()+'})';
 
 	try {
 		var ast = parse(sample);
-//		console.log(printNode(ast)) ;
 
 		var newAst = transform({
+			// Input: the ast and filename
 			filename:fileName,
 			ast:ast
 		},{
+			// Code generation options
 			es6target:false,
 			noRuntime:true,
 			babelTree:true,
-			$arguments:'$args',
-			generatedSymbolPrefix:'$',
 			engine:false,
 			generators:false,
 			promises:true,
 			lazyThenables:false,
 			wrapAwait:true,
+//			$runtime:'_asyncRuntime',
+			$Promise:'Promise',
+			$arguments:'$args',
+			generatedSymbolPrefix:'$',
 			$return:'$return',
 			$error:'$error'
 		},
-		console.log.bind(console),
 		{
-			part:partialParse,
-			treeWalker:treeWalker
-		},
-		printNode
-		).ast ;
+			// Helpers for the transformer:
+			parse: parse,						// Parse a JS fragment into an AST
+			printNode: printNode,				// Print a node as JS source
+			logger:console.log.bind(console)		// Log a warning
+		}).ast ;
 
-		Promise.all([sample,printNode(ast),printNode(newAst)].map(code => eval(code)())).then(
-				r => console.log(fileName,r && r[0]==r[1] && r[1]==r[2]),
-				ex => console.log(fileName,ex)
+		//console.log(printNode(newAst))
+		
+		Promise.all([sample,printNode(ast),printNode(newAst)].map(function(code) {
+			return eval(code)()
+		})).then(
+			function(r) { console.log(fileName,r && r[0]==r[1] && r[1]==r[2]) },
+			function(ex) { console.log(fileName,ex) }
 		) ;
 
-		//console.log(printNode(newAst)) ;
 	} catch (ex) {
 		console.log(fileName,ex) ;
 	}
-}
+})();
